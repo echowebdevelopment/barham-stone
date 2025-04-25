fb463c2d30529bd31f5c1c6ddeb4dfb8043caef4
Michael Mia <michael@echowebsolutions.co.uk>
2025-04-25 23:37:41 +0800

    instagram feed

---
+++ template-parts/partials/acf/blocks/block-instagram_feed.php
@@ -0,0 +1,21 @@
+
+<?php
+    $default = array(
+        "shortcode"                    => get_sub_field( 'shortcode' )
+    );
+    
+    $args = wp_parse_args( $args, $default );
+?>
+
+<div class="block-instagram" data-delay="0.1">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Follow us on Instagram</h2>
+            </div>
+            <div class="col-lg-12">
+                <?php echo do_shortcode(". $args['shortcode'] ."); ?>
+            </div>
+        </div>
+    </div>
+</div> 
