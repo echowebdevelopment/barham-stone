34900f385ab5235e35dc99b611d8c32041c04003
Michael Mia <michael@echowebsolutions.co.uk>
2025-05-21 17:23:52 +0800

    footer layout

--- sidebar-templates/sidebar-footerfull.php
+++ sidebar-templates/sidebar-footerfull.php
@@ -20,9 +20,38 @@ $container = get_theme_mod( 'echo_container_type' );
 
 		<div class="<?php echo esc_attr( $container ); ?>" id="footer-full-content" tabindex="-1">
 
-			<div class="row">
+			<div class="row row-cols-1 row-cols-md-2 row-cols-lg-5 ">
 
-				<?php dynamic_sidebar( 'footerfull' ); ?>
+				<div class="col p-2">
+					<?php get_template_part( 'global-templates/navbar-branding' ); ?>
+				</div>
+				<div class="col p-2">
+					<h3 class="widget-title">Menu</h3>
+					<?php
+						wp_nav_menu(
+							array(
+								'theme_location'  => '',
+								'container_class' => '',
+								'container_id'    => '',
+								'menu_class'      => 'menu',
+								'fallback_cb'     => '',
+								'menu_id'         => 'main-menu',
+								'depth'           => 2,
+							)
+						);
+					?>
+				</div>
+				<div class="col p-2">
+					<?php echo do_shortcode('[footer-nap]'); ?>
+				</div>
+				<div class="col p-2">
+					<?php echo do_shortcode('[social-icons]'); ?>
+					<a href="https://namm.org.uk/" target="_blank"><img src="<?php echo get_template_directory_uri() . '/css/icons/namm.png'; ?>" class="namm-logo"></a>
+				</div>
+				<div class="col p-2 text-white">
+					<a href="/enquiry/" class="btn">make an enquiry</a>
+					<a href="/enquiry/" class="link-text">GO TO WHOLESALE ENQUIRY</a>
+				</div>
 
 			</div>
 
